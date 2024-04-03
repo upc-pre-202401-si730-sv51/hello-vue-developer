86a1396085fe6da3fc4f39779e423603ece92e76
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-02 20:18:22 -0500

    chore: initial commit.

---
+++ src/sales/components/welcome.component.vue
@@ -0,0 +1,26 @@
+<script>
+import {Developer} from "../domain/model/developer.entity.js";
+
+export default {
+  name: "welcome",
+  props: {
+    firstName: String,
+    lastName: String
+  },
+
+  methods: {
+    developerName() {
+      let developer = new Developer(this.firstName, this.lastName);
+      return developer.fullName;
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Hello {{ developerName() }}. Now you are a Vue Developer!</p>
+</template>
+
+<style scoped>
+
+</style>
